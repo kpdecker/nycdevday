8a0fb0ebf375a801b449b5b8a2c03a060c42391d
kpdecker <kpdecker@gmail.com>
2010-11-11 21:30:39 -0600

    Other scene

--- justtype/app/assistants/main-assistant.js
+++ justtype/app/assistants/main-assistant.js
@@ -1,7 +1,17 @@
 function MainAssistant(params) {
+    this.pushOtherSceneHandler = this.pushOtherScene.bindAsEventListener(this);
 };
 
 MainAssistant.prototype = {
     setup: function() {
+        this.controller.setupWidget("push-other-scene", {label: $L("Other Scene")}, {});
+        this.controller.listen("push-other-scene", Mojo.Event.tap, this.pushOtherSceneHandler);
+    },
+    cleanup: function() {
+        this.controller.stopListening("push-other-scene", this.pushOtherSceneHandler);
+    },
+
+    pushOtherScene: function() {
+        this.controller.stageController.pushScene("other");
     },
 };
---
+++ justtype/app/assistants/other-assistant.js
@@ -0,0 +1,3 @@
+function OtherAssistant() {
+    /* Scene assistant is purposefully a NOP to demonstrate the behavior when main is the delegate handler but not topmost. */
+}
