5ff4dffafd789732f1ec09dc5bd6802c9cd03c5b
kpdecker <kpdecker@gmail.com>
2010-11-12 16:14:07 -0600

    Main scene test dock stage button

--- exhibition/app/assistants/main-assistant.js
+++ exhibition/app/assistants/main-assistant.js
@@ -1,9 +1,17 @@
 function MainAssistant() {
+    this.pushDockStageHandler = this.pushDockStage.bindAsEventListener(this);
 };
 
 MainAssistant.prototype = {
     setup: function() {
+        this.controller.setupWidget("push-dock-stage", {label: $L("Push Dock Stage")}, {});
+        this.controller.listen("push-dock-stage", Mojo.Event.tap, this.pushDockStageHandler);
     },
     cleanup: function() {
+        this.controller.stopListening("push-dock-stage", Mojo.Event.tap, this.pushDockStageHandler);
+    },
+
+    pushDockStage: function() {
+        Mojo.Log.info("pushDockStage");
     },
 };
