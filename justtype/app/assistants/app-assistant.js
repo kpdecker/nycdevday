75ef89e1456644d1b2d3ef85a80fdc21d514ae16
kpdecker <kpdecker@gmail.com>
2010-11-12 13:03:41 -0600

    AppAssistant.delegateToSupportingScene

--- justtype/app/assistants/app-assistant.js
+++ justtype/app/assistants/app-assistant.js
@@ -13,4 +13,26 @@ AppAssistant.prototype = {
             },
             Mojo.Controller.StageType.card);
     },
+
+    delegateToSupportingScene: function(stageName, delegateName, params) {
+        var controller = this.controller.getStageController(stageName),
+            proxy = this.controller.getStageProxy(stageName);
+        if (controller) {
+            // The scene if fully initialized, we delegate directly, walking on the scene stack until we
+            // find a scene that supports this operation
+            var scenes = controller.getScenes(),
+                len = scenes.length;
+            for (var i = 0; i < len; i++) {
+                var assistant = scenes[i].assistant,
+                    delegateFn = assistant[delegateName];
+                if (delegateFn) {
+                    delegateFn.call(assistant, params);
+                    break;
+                }
+            }
+        } else if (proxy) {
+            // The scene is in the process of init, delegate using the Mojo API
+            proxy.delegateToSceneAssistant(delegateName, params);
+        }
+    },
 };
