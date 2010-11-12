7f1173d0a0d37e2777e0be508fd31af354c719d3
kpdecker <kpdecker@gmail.com>
2010-11-12 13:04:47 -0600

    Prevent multiple instantiations of the main stage.

--- justtype/app/assistants/app-assistant.js
+++ justtype/app/assistants/app-assistant.js
@@ -4,14 +4,23 @@ function AppAssistant(appController) {
 AppAssistant.prototype = {
     handleLaunch: function(params){
         Mojo.Log.info("handleLaunch: %j", params);
-        this.controller.createStageWithCallback({
-                name: "main",
-                lightweight: true,
-            },
-            function(controller) {
-                controller.pushScene("main", params);
-            },
-            Mojo.Controller.StageType.card);
+
+        // Create the main scene if we do not already have one
+        var proxy = this.controller.getStageProxy("main");
+        if (!proxy) {
+            // The stage controller has not been created
+            this.controller.createStageWithCallback({
+                    name: "main",
+                    lightweight: true,
+                },
+                function(controller) {
+                    controller.pushScene("main");
+                },
+                Mojo.Controller.StageType.card);
+        } else {
+            var controller = this.controller.getStageController("main");
+            controller && controller.activate();
+        }
     },
 
     delegateToSupportingScene: function(stageName, delegateName, params) {
