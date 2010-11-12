e29deaa5a3354d4c819817b6d1964f820f114100
kpdecker <kpdecker@gmail.com>
2010-11-12 15:52:38 -0600

    Exhibition core files

---
+++ exhibition/app/assistants/app-assistant.js
@@ -0,0 +1,28 @@
+function AppAssistant() {}
+
+AppAssistant.prototype = {
+    handleLaunch: function(params){
+        Mojo.Log.info("handleLaunch: %j", params);
+
+        // Create the main scene if we do not already have one
+        this.createOrActivateStage("main", "main", Mojo.Controller.StageType.card);
+    },
+
+    createOrActivateStage: function(stageName, sceneName, stageType) {
+        var proxy = this.controller.getStageProxy(stageName);
+        if (!proxy) {
+            // The stage controller has not been created
+            this.controller.createStageWithCallback({
+                    name: stageName,
+                    lightweight: true,
+                },
+                function(controller) {
+                    controller.pushScene(sceneName);
+                },
+                stageType);
+        } else {
+            var controller = this.controller.getStageController(stageName);
+            controller && controller.activate();
+        }
+    }
+};
