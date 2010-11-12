02f1d85f169dc9117a25d14d74b867ad70a9eeff
kpdecker <kpdecker@gmail.com>
2010-11-12 12:38:18 -0600

    Allow push other scene from status scene

--- justtype/app/assistants/status-assistant.js
+++ justtype/app/assistants/status-assistant.js
@@ -1,2 +1,17 @@
 function StatusAssistant(params) {
+    this.pushOtherSceneHandler = this.pushOtherScene.bindAsEventListener(this);
 }
+
+StatusAssistant.prototype = {
+    setup: function(params) {
+        this.controller.setupWidget("push-status-other-scene", {label: $L("Other Scene")}, {});
+        this.controller.listen("push-status-other-scene", Mojo.Event.tap, this.pushOtherSceneHandler);
+    },
+    cleanup: function() {
+        this.controller.stopListening("push-status-other-scene", Mojo.Event.tap, this.pushOtherSceneHandler);
+    },
+
+    pushOtherScene: function() {
+        this.controller.stageController.pushScene("other");
+    },
+};
