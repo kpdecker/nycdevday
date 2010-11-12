1d567f7c0aab5d9d331f814016fbd981955f76f3
kpdecker <kpdecker@gmail.com>
2010-11-11 21:43:42 -0600

    Basic main scene

--- justtype/app/assistants/main-assistant.js
+++ justtype/app/assistants/main-assistant.js
@@ -1,9 +1,13 @@
 function MainAssistant(params) {
+    this.pushStatusSceneHandler = this.pushStatusScene.bindAsEventListener(this);
     this.pushOtherSceneHandler = this.pushOtherScene.bindAsEventListener(this);
 };
 
 MainAssistant.prototype = {
     setup: function() {
+        this.controller.setupWidget("push-status-scene", {label: $L("Status Scene")}, {});
+        this.controller.listen("push-status-scene", Mojo.Event.tap, this.pushStatusSceneHandler);
+
         this.controller.setupWidget("push-other-scene", {label: $L("Other Scene")}, {});
         this.controller.listen("push-other-scene", Mojo.Event.tap, this.pushOtherSceneHandler);
     },
@@ -11,6 +15,9 @@ MainAssistant.prototype = {
         this.controller.stopListening("push-other-scene", this.pushOtherSceneHandler);
     },
 
+    pushStatusScene: function() {
+        this.controller.stageController.pushScene("status");
+    },
     pushOtherScene: function() {
         this.controller.stageController.pushScene("other");
     },
