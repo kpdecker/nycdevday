7ac1259f224fe1e7f577198d0d2b62f3a34313a1
kpdecker <kpdecker@gmail.com>
2010-11-15 13:49:15 -0600

    Card Group base implementation

---
+++ cardgroups/app/assistants/app-assistant.js
@@ -0,0 +1,24 @@
+function AppAssistant() {}
+
+AppAssistant.prototype = {
+    handleLaunch: function(params){
+        Mojo.Log.info("handleLaunch: %j", params);
+
+        // Create the main scene if we do not already have one
+        var proxy = this.controller.getStageProxy("main");
+        if (!proxy) {
+            // The stage controller has not been created
+            this.controller.createStageWithCallback({
+                    name: "main",
+                    lightweight: true,
+                },
+                function(controller) {
+                    controller.pushScene("main");
+                },
+                Mojo.Controller.StageType.card);
+        } else {
+            var controller = this.controller.getStageController("main");
+            controller && controller.activate();
+        }
+    },
+};
---
+++ cardgroups/app/assistants/main-assistant.js
@@ -0,0 +1,9 @@
+function MainAssistant() {
+};
+
+MainAssistant.prototype = {
+    setup: function() {
+    },
+    cleanup: function() {
+    },
+};
