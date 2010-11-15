55fee9c710bdaa85ce47fbd3db16e541f44b448d
kpdecker <kpdecker@gmail.com>
2010-11-15 14:09:22 -0600

    Output a "unique" ID in the main scene

--- cardgroups/app/assistants/main-assistant.js
+++ cardgroups/app/assistants/main-assistant.js
@@ -1,8 +1,11 @@
-function MainAssistant() {
+function MainAssistant(unique) {
+    this.unique = unique || (Math.random()*1000)|0;
 };
 
 MainAssistant.prototype = {
     setup: function() {
+        var uniqueEl = this.controller.sceneElement.querySelector(".unique-id");
+        uniqueEl.textContent = $L("Unique Id: #{unique}").interpolate({ unique: this.unique });
     },
     cleanup: function() {
     },
