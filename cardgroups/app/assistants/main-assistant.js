7631690d0586134aac59fb4a2fea2c65dbb03951
kpdecker <kpdecker@gmail.com>
2010-11-15 14:10:37 -0600

    Allow main scene push from main scene.

--- cardgroups/app/assistants/main-assistant.js
+++ cardgroups/app/assistants/main-assistant.js
@@ -1,12 +1,21 @@
 function MainAssistant(unique) {
     this.unique = unique || (Math.random()*1000)|0;
+    this.pushSceneHandler = this.pushScene.bindAsEventListener(this);
 };
 
 MainAssistant.prototype = {
     setup: function() {
         var uniqueEl = this.controller.sceneElement.querySelector(".unique-id");
         uniqueEl.textContent = $L("Unique Id: #{unique}").interpolate({ unique: this.unique });
+
+        this.controller.setupWidget("push-scene", {label: $L("Push New Scene")}, {});
+        this.controller.listen("push-scene", Mojo.Event.tap, this.pushSceneHandler);
     },
     cleanup: function() {
+        this.controller.stopListening("push-scene", Mojo.Event.tap, this.pushSceneHandler);
+    },
+
+    pushScene: function() {
+        this.controller.stageController.pushScene("main");
     },
 };
