a3775744d4a912f4ebadea22a9b8f818fa0d63ce
kpdecker <kpdecker@gmail.com>
2010-11-16 22:55:03 -0600

    Exhibition sample data

---
+++ exhibition/app/models/data-model.js
@@ -0,0 +1,19 @@
+var DataModel = [
+    {
+        name: "Facebook for webOS",
+        pic_square: "http://profile.ak.fbcdn.net/hprofile-ak-snc4/hs224.ash2/50284_4620273157_2303155_q.jpg",
+        src: "http://profile.ak.fbcdn.net/hprofile-ak-snc4/hs224.ash2/50274_4620273157_7312020_n.jpg",
+        parsedMessage: "Lorem ipsum dolor sit amet, consectetur adipisicing elit",
+    },
+    {
+        name: "Kevin Decker",
+        pic_square: "http://profile.ak.fbcdn.net/hprofile-ak-snc4/hs341.snc4/41368_1927962_8276_q.jpg",
+        src: "http://platform.ak.fbcdn.net/www/app_full_proxy.php?app=86734274142&v=1&size=z&cksum=a7199993d4c443fde577d2d7bc7113f2&src=http%3A%2F%2Ffoursquare.com%2Fmapproxy%2F41.888098%2F-87.629431%2Fmap.png",
+        parsedMessage: "Lorem ipsum dolor sit amet, consectetur adipisicing elit",
+    },
+    {
+        name: "Palm",
+        pic_square: "http://profile.ak.fbcdn.net/hprofile-ak-snc4/hs429.ash2/71157_32915930478_7178993_q.jpg",
+        parsedMessage: "NYC Developer Day!",
+    },
+];
