be69597941471179dfe5c2b08a10ef69b525b07a
kpdecker <kpdecker@gmail.com>
2010-11-16 22:58:25 -0600

    Dock item template

--- exhibition/app/assistants/dock-assistant.js
+++ exhibition/app/assistants/dock-assistant.js
@@ -4,5 +4,17 @@ DockAssistant.prototype = {
     setup: function() {
     },
     cleanup: function() {
+    renderEntry: function(result, flyState) {
+        // Render the thing
+        this.controller.sceneElement.innerHTML = Mojo.View.render({
+            template: "dock/item-template",
+            object: {
+                flyState: flyState,
+                pic_square: result.pic_square,
+                name: result.name,
+                content: result.dockContent,
+            }
+        });
+    },
     },
 };
