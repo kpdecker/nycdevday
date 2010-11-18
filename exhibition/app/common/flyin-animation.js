227797b5c0b57e11bd4efcc5050ede2abee27107
kpdecker <kpdecker@gmail.com>
2010-11-18 13:00:30 -0600

    Flyin animation reset state API

--- exhibition/app/common/flyin-animation.js
+++ exhibition/app/common/flyin-animation.js
@@ -35,10 +35,31 @@ FlyinAnimation.prototype = {
             return "";
         }
     },
+    resetAllToState: function(pos) {
+        var state = this.getState(pos),
+            els = this.el.querySelectorAll(getClassQuery(this.elementClassName)),
+            len = els.length;
+        var states = {
+            visible: getClassNameApply(state.indexOf("visible") > -1),
+            flyout: getClassNameApply(state.indexOf("flyout") > -1)
+        };
+
+        for (var i = 0; i < len; i++) {
+            // Reset all of the elements to the desired state
+            var el = els[i];
+            el.addClassName("snap-to");
+            states.flyout.call(el, "flyout");
+            states.visible.call(el, "visible");
+            el.removeClassName("snap-to");
+        }
+    },
 };
 
 function getClassQuery(className, negate) {
     return negate ? (":not(." + className + ")") : ("." + className);
+}
+function getClassNameApply(remove) {
+    return remove ? Element.removeClassName : Element.addClassName;
 }
 
 function flyElement(anim, className, query, complete) {
