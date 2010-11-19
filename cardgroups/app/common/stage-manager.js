637a744daaeb0fea8c67ec15d32221315ea0da2c
kpdecker <kpdecker@gmail.com>
2010-11-19 15:51:20 -0600

    Stage manager documentation

--- cardgroups/app/common/stage-manager.js
+++ cardgroups/app/common/stage-manager.js
@@ -5,14 +5,31 @@ var StageManager;
     var stageCounter = 0,
         appController;
 
+    /**
+     * Helper methods for creating and managing stages and scenes.
+     */
     StageManager = {
+        /**
+         * Init method. Must be called during or after the app assistant setup.
+         */
         init: function() {
             appController = Mojo.Controller.getAppController();
         },
+
+        /**
+         * Unique stage name generator.
+         */
         getStageName: function() {
             return "facebookStage-"+(stageCounter++);
         },
 
+        /**
+         * Helper method that determines if the the Orange/White key or the Meta key was pressed during a tap event.
+         *
+         * @param stageController Current stage controller
+         * @param event Event to examine
+         * @return stageController if either key was not pressed, undefined if a modified key was pressed.
+         */
         stageControllerFromEvent: function(stageController, event) {
             if (event.originalEvent) {
                 // listTap Event
@@ -22,11 +39,19 @@ var StageManager;
                 // tap Event
                 event = event.up;
             }
+
             if (!event.metaKey && !event.altKey) {
                 return stageController;
             }
         },
 
+        /**
+         * Create a scene, optionally creating in it a new stage.
+         *
+         * @param stageController StageController to push the scene to. If undefined, create a new stage.
+         * @param sceneName Name of the scene to push.
+         * @param arguments... Arguments passed to the pushScene method
+         */
         pushOrCreateScene: function(stageController, sceneName) {
             var args = Array.prototype.slice.call(arguments, 1);
             if (!stageController) {
@@ -38,6 +63,10 @@ var StageManager;
                 stageController.pushScene.apply(stageController, args);
             }
         },
+
+        /**
+         * Helper utility for creating a new card stage
+         */
         createStage: function(onInit) {
             Mojo.Log.info("StageManager.createStage");
             var name = StageManager.getStageName();
@@ -49,12 +78,24 @@ var StageManager;
                 Mojo.Controller.StageType.card);
         },
 
+        /**
+         * Clones the given scene, pushing in the destination stage.
+         *
+         * @param sceneController SceneController Scene to clone
+         * @param destinationController StageController Stage to push the cloned scene on to.
+         */
         cloneSceneIntoStage: function(sceneController, destinationController) {
             if (!sceneController._sceneCloneArgs) {
                 throw new Error("Scene missing clone args: " + sceneController.sceneName);
             }
             destinationController.pushScene.apply(destinationController, sceneController._sceneCloneArgs);
         },
+
+        /**
+         * Clones the given scene into a new stage.
+         *
+         * @param sceneController SceneController Scene to clone
+         */
         cloneSceneIntoNewStage: function(sceneController) {
             StageManager.createStage(
                 function(stageController) {
