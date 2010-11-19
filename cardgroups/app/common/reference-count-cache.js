9ba3a10c210a257ec67a9320ebac738b7c179adc
kpdecker <kpdecker@gmail.com>
2010-11-19 15:58:23 -0600

    ReferenceCountCache documentation

--- cardgroups/app/common/reference-count-cache.js
+++ cardgroups/app/common/reference-count-cache.js
@@ -1,7 +1,19 @@
 /* Copyright 2010 Palm, Inc. All rights reserved. */
+
+/**
+ * Cache with reference-counted element removal.
+ */
 var ReferenceCountCache = function() {
     var cache = {};
 
+    /**
+     * Retrieve the element with the given name, if one exists.
+     * This method will increment the reference count for the element and
+     * must have a matching release call.
+     *
+     * @param name String Cache element reference.
+     * @return Value stored in the cache
+     */
     this.get = function(name) {
         var entry = cache[name];
         if (entry) {
@@ -9,9 +21,26 @@ var ReferenceCountCache = function() {
             return entry.value;
         }
     };
+
+    /**
+     * Insert an element into the cache. This element will have a refrence
+     * count number of one and must be paired with a release call to remove
+     * the element from the cache and allow for garbage collection.
+     *
+     * @param name String Cache element reference
+     * @param value Object Value to store in the cache
+     */
     this.set = function(name, value) {
         cache[name] = { value: value, count: 1 };
     };
+
+    /**
+     * Decreases the reference count for the given element. If the count drops
+     * to zero then the object will be removed from the cache and available for
+     * garbage collection.
+     *
+     * @param name String Cache element reference
+     */
     this.release = function(name) {
         var entry = cache[name];
         if (entry) {
